b305f3d4bbd38c2993d6da1539a87648937b7c45
11t518s <11t518s20161759@gmail.com>
2024-01-17 00:40:36 +0900

    first commit

---
+++ src/main/kotlin/com/ppc/myfc/MyfcApplication.kt
@@ -0,0 +1,11 @@
+package com.ppc.myfc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyfcApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyfcApplication>(*args)
+}
---
+++ src/test/kotlin/com/ppc/myfc/MyfcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ppc.myfc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyfcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
